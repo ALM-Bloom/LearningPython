850727897b6d050819cc665153acdbd361a9cadb
AlejandroML <72447092+ALM-Bloom@users.noreply.github.com>
2025-02-23 00:13:36 +0000

    Aprendiendo a usar listas

---
+++ listas.py
@@ -0,0 +1,8 @@
+milista = []
+milista.append(5)
+milista.append("Palabra") # WoW! Las listas pueden conformarse por varios tipos de variables!
+milista.append(45)
+
+
+for x in milista:
+    print(x)
