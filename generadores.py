8c0be81b968e7e77dd39205ad06dacfc26dea669
AlejandroML <72447092+ALM-Bloom@users.noreply.github.com>
2025-02-24 19:08:47 +0000

    Aprendiendo sobre generadores

---
+++ generadores.py
@@ -0,0 +1,14 @@
+# Los Generadores son usados para crear iteradores.
+# Funciones que devuelven un conjunto iterable de items, uno al momento, de una manera especial.
+import random
+
+def lottery():
+    # returns 6 numbers between 1 and 40
+    for i in range(6):
+        yield random.randint(1, 40)
+
+    # returns a 7th number between 1 and 15
+    yield random.randint(1, 15)
+
+for random_number in lottery():
+       print("And the next number is... %d!" %(random_number))
