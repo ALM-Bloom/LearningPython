0b0d6835f7905d0466ed434fbc7e15d06c55d297
AlejandroML <72447092+ALM-Bloom@users.noreply.github.com>
2025-02-24 19:03:53 +0000

    Aprendiendo a tomar valores

---
+++ Input&Output.py
@@ -0,0 +1,14 @@
+# input() anteriormente conocido como raw_input()
+
+# Prints out the input received from stdin
+astring = input() # give hello as input
+print(input())
+
+# Después de recibir el input podremos convertirlo al tipo que queramos
+num = int(input())
+print(num)
+
+decimalnum = input()
+decimalnum = float(input())
+
+print(decimalnum)
